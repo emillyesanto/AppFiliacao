38fcc4366aa8913f84ec7f626f390aada62769b7
Juh Cipriani <julia_cipriani150@estudante.sesisenai.org.br>
2025-10-15 15:28:42 -0300

    Controller Mãe

--- src/main/java/com/psii/appfiliacao/controller/MaeController.java
+++ src/main/java/com/psii/appfiliacao/controller/MaeController.java
@@ -1,5 +1,10 @@
 package com.psii.appfiliacao.controller;
 
-public class MaeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/maes")
+public class MaeController {
+    
 }
