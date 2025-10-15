2e5da989af95d30028d0e47ddcd145c879dd8a42
Juh Cipriani <julia_cipriani150@estudante.sesisenai.org.br>
2025-10-15 14:22:39 -0300

    Atualizando Controller

--- src/main/java/com/psii/appfiliacao/controller/PaiController.java
+++ src/main/java/com/psii/appfiliacao/controller/PaiController.java
@@ -1,5 +1,28 @@
 package com.psii.appfiliacao.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.psii.appfiliacao.model.Pai;
+import com.psii.appfiliacao.service.PaiService;
+
+@Controller
+@RequestMapping("/pais")
 public class PaiController {
+
+    @Autowired
+    private PaiService paiService;
+
+    @GetMapping
+    public String listaPai(Model model) {
+        List<Pai> pai = paiService.listarPai();
+        model.addAttribute("pai", pai);
+        return "lista-pais";
+    }
 
 }
